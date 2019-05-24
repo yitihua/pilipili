f8af14918cbf53ad33cee01002748a853d11c223
dandelionClaw <dandelion18@sina.cn>
2019-05-24 23:57:03 +0800

    commit

---
+++ src/main/java/com/unknown/pilipili/domain/News.java
@@ -0,0 +1,63 @@
+package com.unknown.pilipili.domain;
+
+import com.unknown.pilipili.config.orm.IdEntity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+/**
+ * @author <b>顾思宇</b>
+ * @version 1.0, 2019/5/24 23:40
+ */
+@Entity
+@Table(name = "T_NEWS")
+public class News extends IdEntity {
+
+    private String title;
+
+    @ManyToOne(cascade= CascadeType.ALL,fetch= FetchType.EAGER)
+    @JoinColumn(name="author",referencedColumnName="id")
+    private User author;
+    @ManyToOne(cascade= CascadeType.ALL,fetch= FetchType.EAGER)
+    @JoinColumn(name="type",referencedColumnName="id")
+    private Type type;
+
+    private LocalDateTime createAt = LocalDateTime.now();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+}
--- src/main/java/com/unknown/pilipili/domain/User.java
+++ src/main/java/com/unknown/pilipili/domain/User.java
@@ -1,10 +1,46 @@
 package com.unknown.pilipili.domain;
 
+import com.unknown.pilipili.config.orm.IdEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
 /**
  * @author <b>顾思宇</b>
  * @version 1.0, 2019/5/24 22:07
  */
+@Entity
+@Table(name = "T_USER")
+public class User extends IdEntity {
+    @Column(length = 128,unique = true,nullable = false)
+    private String username;
+    @Column(length = 128,nullable = false)
+    private String password;
+    private LocalDateTime createAt = LocalDateTime.now();
 
-public class User {
-    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
 }
