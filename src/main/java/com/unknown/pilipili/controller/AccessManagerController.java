2904df6c3c0c38d7fcd5f4820fbcd840de562332
Your Name <1072552802@qq.com>
2019-06-03 20:41:56 +0800

    上传栏目管理controller

---
+++ src/main/java/com/unknown/pilipili/controller/AccessManagerController.java
@@ -0,0 +1,22 @@
+package com.unknown.pilipili.controller;
+
+import com.unknown.pilipili.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author <b>zhangfei/b>
+ * @version 1.0, 2019/6/1 20:27
+ */
+@Controller
+@RequestMapping("/accessManager")//这个地方改成jsp文件的名字且不加后缀名
+public class AccessManagerController {//类名和java文件名一样
+    @Autowired
+    private AccountService accountService;
+    @RequestMapping("")
+    public String viewArticle(Model model){
+        return "/accessManager";//这个地方也改成jsp文件的名字且不加后缀名
+    }
+}
