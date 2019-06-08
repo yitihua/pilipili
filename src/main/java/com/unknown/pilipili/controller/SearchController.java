7ee055d54fbf56fb68c95cac62a4d48a9f0747fb
dandelionClaw <dandelion18@sina.cn>
2019-06-08 18:04:16 +0800

    完成搜索功能

--- src/main/java/com/unknown/pilipili/controller/SearchController.java
+++ src/main/java/com/unknown/pilipili/controller/SearchController.java
@@ -1,18 +1,31 @@
 package com.unknown.pilipili.controller;
 
+import com.unknown.pilipili.domain.News;
 import com.unknown.pilipili.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletRequest;
+import java.util.List;
+
+/**
+ * @author <b>顾思宇</b>
+ * @version 1.0, 2019/6/8 17:55
+ */
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
     @Autowired
     private NewsService newsService;
-    @RequestMapping("")
-    public String viewArticle(Model model){
-        return "/search";//这个地方也改成jsp文件的名字且不加后缀名
+    @PostMapping("")
+    public String show(Model model, ServletRequest request){
+        String search = request.getParameter("search");
+        List<News> newsList = newsService.findByTitleLike(search);
+        model.addAttribute("newsList",newsList);
+        return "/search";
     }
 }
