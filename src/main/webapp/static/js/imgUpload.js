28aeee1e412312594ed36f812db01877139c3a7e
BUZA <3396295683@qq.com>
2019-06-14 20:44:31 +0800

    fix:头像上传与头图上传阻止图片以外格式的文件提交

--- src/main/webapp/static/js/imgUpload.js
+++ src/main/webapp/static/js/imgUpload.js
@@ -1,5 +1,11 @@
 var field = document.querySelector('.upload-field');
 var fileBtn = document.getElementById("input-file")
+var fileTip= document.getElementById("upload-tip")
+if(document.getElementById("avatar-submit")){
+    var fileSubmit = document.getElementById("avatar-submit")
+}else {
+    var fileSubmit =document.getElementById("btn1")
+}
 var img = document.getElementById("show-img")
 field .ondragover = function (e) {
     e.preventDefault();
@@ -10,26 +16,40 @@ field .ondrop = function (e) {
     // 1.file对象
     console.log(e.dataTransfer.files);
     var file = e.dataTransfer.files.item(0);
-    fileBtn.files[0]=file
-    console.log(fileBtn.files[0])
-    // 创建文件读取对象
-    let f = new FileReader();
-    // 读取图片 格式base64
-    f.readAsDataURL(file);
-    f.onload = function(e) {
-        // 格式base64
-        img.src = this.result;
-    };
+    //判断文件是否为图片
+    if(file[0].type=="image/jpeg"||file[0].type=="image/jpg"||file[0].type=="image/png"){
+        fileBtn.files[0]=file
+        // 创建文件读取对象
+        let f = new FileReader();
+        // 读取图片 格式base64
+        f.readAsDataURL(file);
+        f.onload = function(e) {
+            // 格式base64
+            img.src = this.result;
+        };
+        if(fileSubmit.hasOwnProperty("disabled")){
+            fileSubmit.removeAttribute("disabled")
+        }
+    }else {
+        fileTip.innerText="请提交格式为png、jpg或jpeg的图片"
+        fileSubmit.setAttribute("disabled","true")
+    }
     e.preventDefault();
     e.stopPropagation();
     return false;
 }
 function preview() {
     if(fileBtn.files&&fileBtn.files[0]){
-        let f=new FileReader();
-        f.readAsDataURL(fileBtn.files[0])
-        f.onload=function () {
-            img.src = this.result
+        if(fileBtn.files[0].type=="image/jpeg"||fileBtn.files[0].type=="image/jpg"||fileBtn.files[0].type=="image/png"){
+            let f=new FileReader();
+            f.readAsDataURL(fileBtn.files[0])
+            f.onload=function () {
+                img.src = this.result
+            }
+        }else {
+            fileTip.innerText="请提交格式为png、jpg或jpeg的图片"
+            fileSubmit.setAttribute("disabled","true")
         }
+
     }
 }
